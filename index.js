750ebeb86328020ab736f573fd335c17a2bfe5e6
Arindamsk209 <91045455+Arindamsk209@users.noreply.github.com>
2024-09-24 16:22:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 const port = process.env.PORT || 4000;
 
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors({ credentials: true, origin: 'https://fascinating-truffle-d8d0b4.netlify.app' }));
 app.use(express.json());
 app.use(cookieParser());
 
